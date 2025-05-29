6742828d825c90994746130c5ebdd81599a4a9f3
Martin Krasser <krasserm@googlemail.com>
2025-05-29 07:41:51 +0200

    Add basic project rules

--- tasks.py
+++ tasks.py
@@ -29,33 +29,29 @@ def deploy_docs(c):
 
 
 @task
-def test(c, cov=False, cov_report=None):
-    _run_pytest(c, "tests", cov, cov_report)
+def test(c, cov=False):
+    _run_pytest(c, "tests", cov)
 
 
 @task(aliases=["ut"])
-def unit_test(c, cov=False, cov_report=None):
-    _run_pytest(c, "tests/unit", cov, cov_report)
+def unit_test(c, cov=False):
+    _run_pytest(c, "tests/unit", cov)
 
 
 @task(aliases=["it"])
-def integration_test(c, cov=False, cov_report=None):
-    _run_pytest(c, "tests/integration", cov, cov_report)
+def integration_test(c, cov=False):
+    _run_pytest(c, "tests/integration", cov)
 
 
-def _run_pytest(c, test_dir, cov=False, cov_report=None):
-    c.run(f"pytest {test_dir} {_pytest_cov_options(cov, cov_report)}", pty=_use_pty())
+def _run_pytest(c, test_dir, cov=False):
+    c.run(f"pytest -xsv {test_dir} {_pytest_cov_options(cov)}", pty=_use_pty())
 
 
 def _use_pty():
     return platform != "win32"
 
 
-def _pytest_cov_options(use_cov: bool, cov_reports: str | None):
+def _pytest_cov_options(use_cov: bool):
     if not use_cov:
         return ""
-
-    cov_report_types = cov_reports.split(",") if cov_reports else []
-    cov_report_types = ["term"] + cov_report_types
-    cov_report_params = [f"--cov-report {r}" for r in cov_report_types]
-    return f"--cov {' '.join(cov_report_params)}"
+    return "--cov=freeact --cov-report=term"
